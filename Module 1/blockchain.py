636f56630bcd57a5912eca61a122ee683e55ead8
Bodimani <MB056330@cerner.net>
2018-07-21 23:14:34 -0500

    Adding block chain

---
+++ Module 1/blockchain.py
@@ -0,0 +1,31 @@
+# Module 1: Create a blockchain
+# To be installed: Flask
+# Postman 
+
+# import libraries
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+# Part 1 - Building a Blockchain
+class Blockchain:
+    
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = '0')
+        
+    def create_block(self, proof, previous_hash):
+        block = {'index' : len(self.chain) +1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof' : proof,
+                 'previous_hash' : previous_hash}
+        self.chain.append(block)
+        return block
+    
+    def get_previous_block(self):
+        return self.chain[-1]
+    
+    def proof_of_work(self, previous_proof)
+
+# Part 2 - Mining iur Blockchain
